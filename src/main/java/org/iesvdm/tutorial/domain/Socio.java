ad3fa26543fb896873ebf3cd81c82874767310e6
Cristian-Serrano <gcristiag@gmail.com>
2024-02-12 14:34:32 +0100

    empezado ejercicio1

--- src/main/java/org/iesvdm/tutorial/domain/Socio.java
+++ src/main/java/org/iesvdm/tutorial/domain/Socio.java
@@ -1,6 +1,23 @@
 package org.iesvdm.tutorial.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Socio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     public long id;
     public String dni;
     public String nombre;
